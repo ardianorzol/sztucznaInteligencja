697f4b31007bfe4a40e010f414a2c1c90ed51644
ardianorzol <32539212+ardianorzol@users.noreply.github.com>
2020-03-03 16:21:29 +0100

    cd

--- zad_a.py
+++ zad_a.py
@@ -1,11 +1,42 @@
 import numpy as np
 
 
+def zamiana(dane, typy):
+    wynik = []
+    dane2 = np.array(dane).transpose()
+    i = 0
+    while i != np.size(typy, 0):
+        if typy[i][1] == 'n':
+            wynik.append(strToInt(dane2[i]))
+        else:
+            wynik.append(dane2[i])
+        i += 1
+    return np.array(wynik).transpose()
+
+
+def strToInt(wiersz):
+    wynik = []
+    for x in wiersz:
+        wynik.append(int(x))
+    return np.array(wynik).transpose()
+
+
 def pierwsze(dane):
     wynik = []
     for x in dane:
         wynik.append(x[-1])
     return list(set(wynik))
+
+
+def drugie(dane):
+    wynik = [0, 0]
+    pom = pierwsze(dane)
+    for x in dane:
+        if x[-1] == pom[0]:
+            wynik[0] += 1
+        else:
+            wynik[1] += 1
+    return wynik
 
 
 typy = []
@@ -17,4 +48,7 @@ with open('diabetes.txt') as plik2:
     for linia in plik2:
         dane.append((linia.strip().split()))
 xd = pierwsze(dane)
-print(xd)
+xdd = drugie(dane)
+dane = zamiana(dane, typy)
+print(strToInt(xd))
+print(xdd)
